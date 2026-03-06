4f543ac7586b803943e8a96f96a2abc5ef2b2a4f
Ankit Johari <jankit.hari2000@gmail.com>
2026-03-06 22:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 
-# load environment variables
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 MODEL_NAME = "gpt-4.1"
